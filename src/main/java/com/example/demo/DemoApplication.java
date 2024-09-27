849168de8e3448985da056b52edf649d344a0594
gripexdev <cj.othman@gmail.com>
2024-09-27 13:45:00 +0100

    Installing Faker, And Saving 20 Random Students

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -17,54 +17,21 @@ public class DemoApplication {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Student maria = new Student(
-                    "Maria",
-                    "Jones",
-                    "maria.jones@gmail.com",
-                    21
-            );
 
-            Student maria2 = new Student(
-                    "Maria",
-                    "Jones",
-                    "maria2.jones@gmail.com",
-                    25
-            );
-
-            Student ahmed = new Student(
-                    "Ahmed",
-                    "Ali",
-                    "ahmed.ali@gmail.com",
-                    18
-            );
-            System.out.println("Adding maria and ahmed");
-            studentRepository.saveAll(List.of(maria, maria2, ahmed));
-
-            //Section-2
-            // Find Student By Email
-            studentRepository
-                    .findStudentByEmail("ahmed.ali@gmail.com")
-                    .ifPresentOrElse(
-                            System.out::println,
-                            () -> System.out.println("Student with email ahmed.ali@gmail.com not found"));
-
-            // find student with name Maria and has age equal to 21
-            studentRepository
-                    .findStudentsByFirstNameEqualsAndAgeEquals(
-                            "Maria",
-                            21
-                    ).forEach(System.out::println);
-
-            // find student with name Maria and has age greater than or equal to 18
-            studentRepository
-                    .findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual(
-                            "Maria",
-                            18
-                    ).forEach(System.out::println);
-
-            // Deleting Maria 2
-            System.out.println(studentRepository.deleteStudentById(3L));
-
+            Faker faker = new Faker();
+            // Saving Random Students Using Faker
+            for (int i = 0; i <= 20; i++) {
+                String firstName = faker.name().firstName();
+                String lastName = faker.name().lastName();
+                String email = String.format("%s.%s@gmail.com", firstName, lastName);
+                Student student = new Student(
+                        firstName,
+                        lastName,
+                        email,
+                        faker.number().numberBetween(17, 55)
+                );
+                studentRepository.save(student);
+            }
         };
     }
 }
